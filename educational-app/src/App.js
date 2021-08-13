122e80b8282f206620a7505dd44feb8172689da1
AdityaShelke29 <adityashelke2014@gmail.com>
2021-08-13 16:49:19 -0400

    Commit
    Everything's set up

---
+++ educational-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  
+  return(
+    <div>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default App;
