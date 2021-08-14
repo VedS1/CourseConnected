9747f06b8bbbe1f6d0575d2cc8553b055ad7e761
Yash Sakharkar <yash.sakharkar0327@gmail.com>
2021-08-14 13:39:33 -0400

    Add basic react frontend for login page
    login page

--- educational-app/src/App.js
+++ educational-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import CourseCreationPage from './components/coursecreation/CourseCreationPage';
+import Login from './components/loginAndRegister/Login';
 
 const App = () => {
   
   return(
     <div>
-      <CourseCreationPage/>
+      <Login />
     </div>
   )
 }
