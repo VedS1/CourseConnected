bfe7fb3e5a6091e4fc2beaf83a4cc5a6c84de0b7
Aryan <aryanovalekar@gmail.com>
2021-08-13 23:32:42 -0400

    Add Bookmark Images Locally

--- educational-app/src/components/Bookmark.js
+++ educational-app/src/components/Bookmark.js
@@ -1,8 +1,11 @@
+import filledBookmark from './bookmark-filled.png';
+import unfilledBookmark from './bookmark-unfilled.png';
+
 const Bookmark = ({showBookmark, onClick}) => {
     return (
         <div>
             <button className='bookmark-toggle' onClick={onClick}>
-                <img src={showBookmark ? "https://cdn.discordapp.com/attachments/874495990575530014/875924896893513728/bookmark-filled.png" : "https://cdn.discordapp.com/attachments/874495990575530014/875924903080132619/bookmark-unfilled.png"}  alt= 'icon' />
+                <img src={showBookmark ? (filledBookmark) : (unfilledBookmark)}/>
             </button>
         </div>
     )
