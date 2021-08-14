44a9949d0067772c2edb009ca2fd7f33f6d77ce7
Aryan <aryanovalekar@gmail.com>
2021-08-14 15:10:33 -0400

    Make changes

--- educational-app/src/App.js
+++ educational-app/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import MenuCard from './components/MenuCard';
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, useParams } from 'react-router-dom'
 
 const App = () => {
   
+  const GetPageID = () => {
+    const { page_id } = useParams()
+    console.log(page_id)
+    return page_id
+  }
+
+  const ClickButton = () => {
+    console.log(useParams())
+  }
+  
   return(
-    <div>
-      <MenuCard />
-    </div>
-  )
-}
+    <Router>
+  
+      <Route exact path="/">
+        <div>
+          <MenuCard />
+          <a href="/test">Test</a>
+        </div>
+      </Route>
+  
+      <Route exact path="/:page_id">
+        <GetPageID id="pageID"/>
+        <h1>{GetPageID}</h1>
+        
+      </Route>
+  
+    </Router>
+    )
+  }
 
 export default App;
