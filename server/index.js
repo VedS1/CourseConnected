f422736caa8c8a38cb6d3261d3ac6f7a5a9a51c4
Ved_ <ved.soolgiri@gmail.com>
2021-08-14 01:29:08 -0400

    Finished backend
    finished adding course creation in backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const moongose = require ("mongoose");
+const cors = require("cors");
 const app = express();
 
 
@@ -7,12 +8,23 @@ const CourseModel = require("./models/Course");
 
 
 app.use(express.json());
+app.use(cors());
 
 moongose.connect("mongodb+srv://EduDefault:OnlyLetters@eduapp.zoxor.mongodb.net/course?retryWrites=true&w=majority", {useNewUrlParser:true});
 
 
 app.post("/insert", async (req, res)=>{
-    const course = new CourseModel({userName: "FirstUsername", password: "FirstPassword"});
+
+    
+    const courseName = req.body.foodName;
+    const id = req.body.id;
+    const title = req.body.title; 
+    const subject = req.body. subject; 
+    const  author = req.body.author;
+    const  description = req.body.description;
+    const  level = req.body. level;
+    const dateOfCreate =  req.body. dateOfCreate; 
+    const course = new CourseModel({courseName: courseName, id: id, title: title, subject: subject, author:author, description:description, level:level, dateOfCreate:dateOfCreate});
     try
     {
         await course.save();
