008b42b3a8ea10b837ff81faa82b585eebf9f55e
Ved_ <ved.soolgiri@gmail.com>
2021-08-14 01:16:29 -0400

    Update CourseCreationPage.js

--- educational-app/src/components/coursecreation/CourseCreationPage.js
+++ educational-app/src/components/coursecreation/CourseCreationPage.js
@@ -1,8 +1,21 @@
 import Axios from "axios"
 
+
 const CourseCreationPage = () => {
     const onSubmit = () => {console.log("Submit")} 
-    
+    const addToDB = () =>{
+        Axios.post("https://localhost:3001/insert", {
+            id: 
+            title: 
+            subject: 
+            author: 
+            description: 
+            level:
+            dateOfCreate: 
+        }
+    }
+
+
     return (
         <div>
             <h1>Course Creation</h1>
