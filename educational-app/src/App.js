b0e750be8a51e15cfb7b66c5cd92bf35870460ca
Aryan <aryanovalekar@gmail.com>
2021-08-14 15:40:32 -0400

    Fix issues with GetPageID

--- educational-app/src/App.js
+++ educational-app/src/App.js
@@ -3,36 +3,29 @@ import MenuCard from './components/MenuCard';
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, useParams } from 'react-router-dom'
 
+//returns id when on url/page/id (Ask Aryan if you don't understand)
+const Page = () => {
+  const str = String(window.location.pathname).slice(6,)
+  return(str)
+}
+
 const App = () => {
   
-  const GetPageID = () => {
-    const { page_id } = useParams()
-    console.log(page_id)
-    return page_id
-  }
-
-  const ClickButton = () => {
-    console.log(useParams())
-  }
+return(
+  <Router>
   
-  return(
-    <Router>
+    <Route exact path="/">
+      <div>
+        <MenuCard />
+        <a href="/test">Test</a>
+      </div>
+    </Route>
   
-      <Route exact path="/">
-        <div>
-          <MenuCard />
-          <a href="/test">Test</a>
-        </div>
-      </Route>
-  
-      <Route exact path="/:page_id">
-        <GetPageID id="pageID"/>
-        <h1>{GetPageID}</h1>
-        
-      </Route>
+    <Route exact path="/page/:page_id">
+      <button onClick={Page}>bruh</button>
+    </Route>
   
     </Router>
-    )
-  }
+)}
 
 export default App;
