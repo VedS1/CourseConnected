018d1f784addac545a28b591f4cba6b0db9f9275
Aryan <aryanovalekar@gmail.com>
2021-08-15 00:50:10 -0400

    Update Menubar

--- educational-app/src/components/loginAndRegister/Login.js
+++ educational-app/src/components/loginAndRegister/Login.js
@@ -43,7 +43,7 @@ const Login = () => {
                     <label>Password</label>
                     <input type='password' placeholder = 'Add Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
-                <input type = 'submit' value =  'Save Task' className='btn btn-block'></input>
+                <input type = 'submit' value =  'Login' className='btn btn-block'></input>
             </form>
             <div>
                 <p>
